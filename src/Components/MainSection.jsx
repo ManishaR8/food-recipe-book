9d8d0a5d2ec631b6ef7e8774bbc710116a1f3281
ManishaR8 <109299159+ManishaR8@users.noreply.github.com>
2024-04-09 13:08:38 +0530

    Update MainSection.jsx

--- src/Components/MainSection.jsx
+++ src/Components/MainSection.jsx
@@ -5,7 +5,7 @@ const MainSection = () => {
 
     let [recipes, setRecipes] = useState([]);
 
-    const API_KEY = "04b2e36c7f1042c9bfc6ce908a0d21e2";
+    const API_KEY = "043d2493cfdb4dcfa156a1afc4711d7a";
 
     useEffect(() => {
         const getRecipe = async () => {
